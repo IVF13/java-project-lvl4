d1d02a1502369d96efe80ea872be7a850fa6733e
i.gordienko <i.gordienko@ylab.io>
2022-05-17 21:07:46 +0300

    Add getApp method

--- src/main/java/hexlet/code/App.java
+++ src/main/java/hexlet/code/App.java
@@ -1,7 +1,23 @@
 package hexlet.code;
 
+import io.javalin.Javalin;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Javalin app = getApp();
+        app.start(8080);
+        app.get("/", ctx -> ctx.result("Hello World"));
     }
+
+    public static Javalin getApp() {
+
+        // Создаём приложение
+        Javalin app = Javalin.create(config -> {
+            // Включаем логгирование
+            config.enableDevLogging();
+        });
+
+        return app;
+    }
+
 }
