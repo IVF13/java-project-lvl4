f1556a792a51c13ee98f475fc54808e5914959ea
IVF13 <iw.gordienko@yandex.ru>
2022-05-18 17:49:31 +0300

    Heroku

--- src/main/java/hexlet/code/App.java
+++ src/main/java/hexlet/code/App.java
@@ -5,7 +5,7 @@ import io.javalin.Javalin;
 public class App {
     public static void main(String[] args) {
         Javalin app = getApp();
-        app.start(8080);
+        app.start(5000);
         app.get("/", ctx -> ctx.result("Hello World!"));
     }
 
