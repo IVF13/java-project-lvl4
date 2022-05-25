43fc90621c8a7d508148fd41317da59a191abd81
IVF13 <iw.gordienko@yandex.ru>
2022-05-25 17:21:01 +0300

    5th step

--- src/main/java/hexlet/code/App.java
+++ src/main/java/hexlet/code/App.java
@@ -1,5 +1,7 @@
 package hexlet.code;
 
+import hexlet.code.controllers.RootController;
+import hexlet.code.controllers.UrlsController;
 import io.javalin.Javalin;
 import io.javalin.plugin.rendering.template.JavalinThymeleaf;
 import nz.net.ultraq.thymeleaf.layoutdialect.LayoutDialect;
@@ -7,32 +9,21 @@ import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.extras.java8time.dialect.Java8TimeDialect;
 import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
 
-public class App {
-    public static void main(String[] args) {
-        Javalin app = getApp();
-        app.start(getPort());
-        app.get("/", ctx -> ctx.result("Hello World!"));
-    }
+import static io.javalin.apibuilder.ApiBuilder.*;
+
+public final class App {
 
     private static int getPort() {
-        String port = System.getenv("PORT");
-        if (port != null) {
-            return Integer.valueOf(port);
-        }
-        return 5000;
+        String port = System.getenv().getOrDefault("PORT", "5000");
+        return Integer.valueOf(port);
     }
 
-    public static Javalin getApp() {
+    private static String getMode() {
+        return System.getenv().getOrDefault("APP_ENV", "development");
+    }
 
-        // Создаём приложение
-        Javalin app = Javalin.create(config -> {
-            // Включаем логгирование
-            config.enableDevLogging();
-            config.enableWebjars();
-            JavalinThymeleaf.configure(getTemplateEngine());
-        });
-
-        return app;
+    private static boolean isProduction() {
+        return getMode().equals("production");
     }
 
     private static TemplateEngine getTemplateEngine() {
@@ -48,4 +39,41 @@ public class App {
         return templateEngine;
     }
 
+    private static void addRoutes(Javalin app) {
+        app.get("/", RootController.welcome);
+        //app.get("/about", RootController.about);
+
+        app.routes(() -> {
+            path("urls", () -> {
+                get(UrlsController.listUrls);
+                post(UrlsController.create);
+                path("{id}", () -> {
+                    get(UrlsController.showUrl);
+                });
+            });
+        });
+    }
+
+    public static Javalin getApp() {
+        Javalin app = Javalin.create(config -> {
+            if (!isProduction()) {
+                config.enableDevLogging();
+            }
+            config.enableWebjars();
+            JavalinThymeleaf.configure(getTemplateEngine());
+        });
+
+        addRoutes(app);
+
+        app.before(ctx -> {
+            ctx.attribute("ctx", ctx);
+        });
+
+        return app;
+    }
+
+    public static void main(String[] args) {
+        Javalin app = getApp();
+        app.start(getPort());
+    }
 }
---
+++ src/main/java/hexlet/code/controllers/UrlsController.java
@@ -0,0 +1,92 @@
+package hexlet.code.controllers;
+
+import hexlet.code.domain.Url;
+import hexlet.code.domain.query.QUrl;
+import io.ebean.PagedList;
+import io.javalin.http.Handler;
+import io.javalin.http.NotFoundResponse;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public final class UrlsController {
+
+    public static Handler listUrls = ctx -> {
+        int page = ctx.queryParamAsClass("page", Integer.class).getOrDefault(1) - 1;
+        int rowsPerPage = 10;
+
+        PagedList<Url> pagedUrls = new QUrl()
+                .setFirstRow(page * rowsPerPage)
+                .setMaxRows(rowsPerPage)
+                .orderBy()
+                .id.asc()
+                .findPagedList();
+
+        List<Url> urls = pagedUrls.getList();
+
+        int lastPage = pagedUrls.getTotalPageCount() + 1;
+        int currentPage = pagedUrls.getPageIndex() + 1;
+        List<Integer> pages = IntStream
+                .range(1, lastPage)
+                .boxed()
+                .collect(Collectors.toList());
+
+        ctx.attribute("urls", urls);
+        ctx.attribute("pages", pages);
+        ctx.attribute("currentPage", currentPage);
+        ctx.render("urls/index.html");
+    };
+
+    public static Handler showUrl = ctx -> {
+        int id = ctx.pathParamAsClass("id", Integer.class).getOrDefault(null);
+
+        Url url = new QUrl()
+                .id.equalTo(id)
+                .findOne();
+
+        if (url == null) {
+            throw new NotFoundResponse();
+        }
+
+        ctx.attribute("url", url);
+        ctx.render("urls/show.html");
+    };
+
+    public static Handler create = ctx -> {
+        URL inputURL;
+
+        try {
+            inputURL = new URL(ctx.formParam("url"));
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+            ctx.sessionAttribute("flash", "Incorrect URL");
+            ctx.sessionAttribute("flash-type", "danger");
+            ctx.render("index.html");
+            return;
+        }
+
+        if (new QUrl().name.equalTo(inputURL.getHost() + inputURL.getPort()).findOne() != null) {
+            ctx.sessionAttribute("flash", "Page already exists");
+            ctx.sessionAttribute("flash-type", "success");
+        } else {
+            String name = inputURL.getProtocol() + "://" + inputURL.getHost();
+            if (inputURL.getPort() != -1) {
+                name += ":" + inputURL.getPort();
+            }
+
+            Url url = new Url();
+            url.setName(name);
+            url.save();
+
+            ctx.sessionAttribute("flash", "Page successfully added");
+            ctx.sessionAttribute("flash-type", "success");
+        }
+
+        ctx.redirect("/urls");
+    };
+
+
+}
