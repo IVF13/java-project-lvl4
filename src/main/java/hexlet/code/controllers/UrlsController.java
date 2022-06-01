73fb82043fd68697edf4874934bcf47294de66f2
IVF13 <iw.gordienko@yandex.ru>
2022-06-01 13:47:52 +0300

    Update for passing tests

--- src/main/java/hexlet/code/controllers/UrlsController.java
+++ src/main/java/hexlet/code/controllers/UrlsController.java
@@ -14,7 +14,6 @@ import org.jsoup.nodes.Document;
 
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.net.UnknownHostException;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
